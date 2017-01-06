2952944497475833295f3d2c9d7fe59062b2761b
sharad <sampat.singh@hexaviewtech.com>
2017-01-06 17:15:47 +0530

    prog 3

---
+++ my_prog/myclass3.java
@@ -0,0 +1,10 @@
+package mypackage;
+class myclass3
+
+{
+	public static void main(String ar[])
+	{
+	System.out.println("Hye Sharad");
+	System.out.println("This is an another progrtam u would like.");
+	}
+}
